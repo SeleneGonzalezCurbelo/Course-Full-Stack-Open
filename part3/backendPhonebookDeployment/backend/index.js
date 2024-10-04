1c101e78c351eb90053caf67facae41165db5cd9
unknown <selene.gonzalez106@alu.ulpgc.es>
2024-10-04 02:23:17 +0100

    part3

--- part3/backendPhonebookDeployment/backend/index.js
+++ part3/backendPhonebookDeployment/backend/index.js
@@ -45,7 +45,7 @@ let persons = [
 ]
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
     });
 
     app.get('/api/persons/:id', (request, response) => {
