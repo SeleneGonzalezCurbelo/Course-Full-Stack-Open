58fc71899e6d9eb5915286177e0bd0240666121b
unknown <selene.gonzalez106@alu.ulpgc.es>
2024-10-04 02:18:44 +0100

    part3

--- part3/backendPhonebookDeployment/backend/index.js
+++ part3/backendPhonebookDeployment/backend/index.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 morgan.token('body', (req) => {
     return req.method === 'POST' ? JSON.stringify(req.body) : ''
