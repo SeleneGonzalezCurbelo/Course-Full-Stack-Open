03e41a444a99524985f82ddda9dc1fd043184c6d
unknown <selene.gonzalez106@alu.ulpgc.es>
2024-10-16 19:48:40 +0100

    part 4 4.4

--- part4/blogsList/App.test.js
+++ part4/blogsList/App.test.js
@@ -9,3 +9,58 @@ describe('Dummy function tests', () => {
     assert.strictEqual(result, 1);
   });
 });
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithMultipleBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'Go To Statement Considered Harmful 2',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://example.com/blog',
+      likes: 3,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f10',
+      title: 'Go To Statement Considered Harmful 3',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://example2.com/blog',
+      likes: 7,
+      __v: 0
+    }
+  ];
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('when list has multiple blogs, equals the sum of likes', () => {
+    const result = listHelper.totalLikes(listWithMultipleBlogs);
+    assert.strictEqual(result, 15); 
+  });
+
+  test('when list is empty, equals zero', () => {
+    const result = listHelper.totalLikes([]);
+    assert.strictEqual(result, 0);
+  });
+})
