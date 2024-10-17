3290740824f1f43d42eb3327737797ec80515078
unknown <selene.gonzalez106@alu.ulpgc.es>
2024-10-17 22:53:12 +0100

    part 4 4.10

--- part4/blogsList/tests/blog_api_test.js
+++ part4/blogsList/tests/blog_api_test.js
@@ -9,7 +9,7 @@ const app = require('../app')
 
 const api = supertest(app)
 
-describe('when there is initially some blogs saved', () => {
+describe('GET /api/blogs', () => {
   beforeEach(async () => {
     await Blog.deleteMany({})
     await Blog.insertMany(helper.initialBlogs)
@@ -21,11 +21,9 @@ describe('when there is initially some blogs saved', () => {
       .expect(200)
       .expect('Content-Type', /application\/json/)
       .catch((err) => console.error(err));
-  })
 
-  test('all blogs are returned', async () => {
-    const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+      const response = await api.get('/api/blogs')
+      assert.strictEqual(response.body.length, helper.initialBlogs.length)
   })
 
   test('check that posts have the id property instead of _id', async () => {
@@ -39,6 +37,78 @@ describe('when there is initially some blogs saved', () => {
   })
 })
 
+
+describe('POST  /api/blogs', () => {
+  const { test, after, beforeEach, describe } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const Blog = require('../models/blog') 
+const assert = require('assert');
+
+const app = require('../app')
+
+const api = supertest(app)
+
+describe('GET /api/blogs', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+  })
+
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      .catch((err) => console.error(err));
+
+      const response = await api.get('/api/blogs')
+      assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+
+  test('check that posts have the id property instead of _id', async () => {
+    const response = await api.get('/api/blogs')
+    const blogs = response.body
+
+    blogs.forEach(blog => {
+      assert.ok(blog.id)
+      assert.strictEqual(blog._id, undefined)
+    })
+  })
+})
+
+
+describe('POST  /api/blogs', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+  })
+
+  const newBlog = {
+    title: 'New Blog Post',
+    author: 'New Author',
+    url: 'http://example.com/newblog',
+    likes: 2
+  };
+
+  test('a valid blog can be added', async () => {
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      .catch((err) => console.error(err));
+
+      const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, helper.initialBlogs.length+1)
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
+})
 
 after(async () => {
   await mongoose.connection.close()
