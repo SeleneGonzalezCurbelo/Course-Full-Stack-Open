7a9e2cd356c3fe8868ba01a60fff45e6106354f5
unknown <selene.gonzalez106@alu.ulpgc.es>
2024-10-19 16:22:02 +0100

    part 4 4.18

--- part4/blogsList/controllers/blogs.js
+++ part4/blogsList/controllers/blogs.js
@@ -4,12 +4,12 @@ const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response, next) => {
   try {
-    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
-    response.json(blogs);
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+    response.json(blogs)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 
 blogsRouter.get('/:id', async (request, response, next) => {
