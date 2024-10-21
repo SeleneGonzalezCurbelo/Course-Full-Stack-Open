dfb2f46de9ec3a005fc1ccf96f334a2e2d67b2ae
unknown <selene.gonzalez106@alu.ulpgc.es>
2024-10-21 21:10:35 +0100

    part 4 4.4

--- part4/blogsList/App.test.js
+++ part4/blogsList/App.test.js
@@ -22,45 +22,45 @@ describe('total likes', () => {
 
   const listWithMultipleBlogs = [
     {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
       likes: 5,
       __v: 0
     },
     {
-      _id: '5a422aa71b54a676234d17f9',
-      title: 'Go To Statement Considered Harmful 2',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://example.com/blog',
-      likes: 3,
-      __v: 0
-    },
-    {
-      _id: '5a422aa71b54a676234d17f10',
-      title: 'Go To Statement Considered Harmful 3',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://example2.com/blog',
-      likes: 7,
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
       __v: 0
     }
   ]
+
+  test('of empty list of zero', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
 
   test('when list has only one blog, equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
     expect(result).toBe(5)
   })
 
-  test('when list has multiple blogs, equals the sum of likes', () => {
+  test('of a bigger list is calculated right', () => {
     const result = listHelper.totalLikes(listWithMultipleBlogs)
-    expect(result).toBe(15)
-  })
-
-  test('when list is empty, equals zero', () => {
-    const result = listHelper.totalLikes([])
-    expect(result).toBe(0)
-  })
+    expect(result).toBe(24)
+  })  
 })
 
 describe('favorite blog', () => {
