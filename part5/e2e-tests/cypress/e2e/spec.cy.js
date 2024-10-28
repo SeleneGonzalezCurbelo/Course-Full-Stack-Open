e12c85861fc33031998c0fabf9bb4d2da043f63e
unknown <selene.gonzalez106@alu.ulpgc.es>
2024-10-28 22:04:48 +0000

    part 5 5.19

--- part5/e2e-tests/cypress/e2e/spec.cy.js
+++ part5/e2e-tests/cypress/e2e/spec.cy.js
@@ -36,4 +36,32 @@ describe('Blog app', function() {
       cy.get('.MessageError').should('have.css', 'color', 'rgb(255, 0, 0)')
     })
   })
+
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.request('POST', 'http://localhost:3001/api/testing/reset')
+      const user = {
+        username: 'userPrueba',
+        name: 'User Test',
+        password: 'prueba'
+      }
+      cy.request('POST', 'http://localhost:3001/api/users', user)
+      
+      cy.visit('http://localhost:5173')
+      cy.contains('Show Login').click()
+      cy.get('#username').type('userPrueba')
+      cy.get('#password').type('prueba')
+      cy.get('#login-button').click()
+    })
+
+    it('A blog can be created', function() {
+      cy.get('#title').type('blogPrueba')
+      cy.get('#author').type('Author Test')
+      cy.get('#url').type('http://localhost:5173/')
+      cy.contains('Create').click()
+      cy.get('.MessageAdd').should('contain', 'A new blog blogPrueba by Author Test')
+      cy.get('.MessageAdd').should('have.css', 'border-style', 'solid')
+      cy.get('.MessageAdd').should('have.css', 'color', 'rgb(0, 128, 0)')
+    })
+  })
 })
